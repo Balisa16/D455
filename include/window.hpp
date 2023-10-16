0517235a493894f3a96a1e1a30bdbcc9495a793a
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-10-17 00:53:12 +0700

    Update CMakeLists

--- include/window.hpp
+++ include/window.hpp
@@ -1,165 +1,32 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
-#include <librealsense2/rs.hpp>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <linmath.h>
+#include <iostream>
 
-struct app_state {
-    app_state(float yaw = 15.0, float pitch = 15.0) : 
-        yaw(yaw), pitch(pitch), last_x(0.0), last_y(0.0),
-        ml(false), offset_x(2.f), offset_y(2.f) {}
-    double yaw;
-    double pitch;
-    double last_x;
-    double last_y;
-    bool ml;
-    float offset_x;
-    float offset_y;
-};
+extern const char *vertexShaderSource;
+extern const char *fragmentShaderSource;
 
 class window
 {
 private:
     GLFWwindow* win;
-    int _width, _height;
-    app_state _state;
+    std::string title;
+    int width, height;
+    unsigned int shader_program, VAO, VBO;
 
+    unsigned int compile_shader();
+    void conf_vertex(unsigned int* VAO, unsigned int* VBO);
+    static void fbuff_callback(GLFWwindow* window, int width, int height);
 public:
-    // operator GLFWwindow* () { return win; }
-    // Mouse callback function
-    std::function<void(bool)>           on_left_mouse = [](bool) {};
-    std::function<void(double, double)> on_mouse_scroll = [](double, double) {};
-    std::function<void(double, double)> on_mouse_move = [](double, double) {};
-    std::function<void(int)>            on_key_release = [](int) {};
-    void mouse_callbacks();
-    window(std::string title, int width = 640, int height = 480);
+    window(std::string title = "D455", int width = 640, int height = 480);
     bool is_open();
-    void refresh();
+    void processinput();
+    void clear_buffer();
+    void draw();
+    void swap_poll();
     ~window();
 };
-
-void window::mouse_callbacks()
-{
-    on_left_mouse = [&](bool pressed)
-    {
-        _state.ml = pressed;
-    };
-
-    on_mouse_scroll = [&](double xoffset, double yoffset)
-    {
-        _state.offset_x -= static_cast<float>(xoffset);
-        _state.offset_y -= static_cast<float>(yoffset);
-    };
-
-    on_mouse_move = [&](double x, double y)
-    {
-        if (_state.ml)
-        {
-            _state.yaw -= (x - _state.last_x);
-            _state.yaw = std::max(_state.yaw, -120.0);
-            _state.yaw = std::min(_state.yaw, +120.0);
-            _state.pitch += (y - _state.last_y);
-            _state.pitch = std::max(_state.pitch, -80.0);
-            _state.pitch = std::min(_state.pitch, +80.0);
-        }
-        _state.last_x = x;
-        _state.last_y = y;
-    };
-
-    on_key_release = [&](int key)
-    {
-        if (key == 32) // Escape
-        {
-            _state.yaw = _state.pitch = 0; 
-            _state.offset_x = _state.offset_y = 0.0;
-        }
-    };
-}
-
-bool window::is_open(){
-    return glfwWindowShouldClose(win);
-}
-
-void window::refresh()
-{   
-    /*glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);*/
-    glfwSwapBuffers(win);
-    glfwPollEvents();
-}
-
-window::window(std::string title, int width, int height) : 
-    _width(width), _height(height)
-{
-    glfwInit();
-    win = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
-    if (!win)
-        throw std::runtime_error("Could not open OpenGL window, please check your graphic drivers or use the textual SDK tools");
-    glfwMakeContextCurrent(win);
-
-    glfwSetWindowUserPointer(win, this);
-    glfwSetMouseButtonCallback(win, [](GLFWwindow* w, int button, int action, int mods)
-        {
-            auto s = (window*)glfwGetWindowUserPointer(w);
-            if (button == 0) s->on_left_mouse(action == GLFW_PRESS);
-        });
-
-    glfwSetScrollCallback(win, [](GLFWwindow* w, double xoffset, double yoffset)
-        {
-            auto s = (window*)glfwGetWindowUserPointer(w);
-            s->on_mouse_scroll(xoffset, yoffset);
-        });
-
-    glfwSetCursorPosCallback(win, [](GLFWwindow* w, double x, double y)
-        {
-            auto s = (window*)glfwGetWindowUserPointer(w);
-            s->on_mouse_move(x, y);
-        });
-
-    glfwSetKeyCallback(win, [](GLFWwindow* w, int key, int scancode, int action, int mods)
-        {
-            auto s = (window*)glfwGetWindowUserPointer(w);
-            if (0 == action) // on key release
-            {
-                s->on_key_release(key);
-            }
-        });
-
-    mouse_callbacks();
-
-    glViewport(0, 0, _width, _height);
-    mat4x4 projection;
-    float ratio = (float)_width / (float)_height;
-    mat4x4_perspective(projection,
-                       60.f * (float) M_PI / 180.f,
-                       ratio,
-                       1.f, 1024.f);
-    glLoadMatrixf((const GLfloat*) projection);
-}
-
-window::~window()
-{
-    glfwDestroyWindow(win);
-    glfwTerminate();
-}
-
-
-// Struct for managing rotation of pointcloud view
-/*struct glfw_state {
-    glfw_state(float yaw = 15.0, float pitch = 15.0) : yaw(yaw), pitch(pitch), last_x(0.0), last_y(0.0),
-        ml(false), offset_x(2.f), offset_y(2.f), tex() {}
-    double yaw;
-    double pitch;
-    double last_x;
-    double last_y;
-    bool ml;
-    float offset_x;
-    float offset_y;
-    texture tex;
-};*/
-
-
-
 
 #endif
---
+++ src/window.cc
@@ -0,0 +1,160 @@
+#include <window.hpp>
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+window::window(std::string title, int width, int height):
+    title(title), width(width), height(height)
+{
+    // Initialize
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    win = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+    if(win == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        throw;
+    }
+
+    glfwMakeContextCurrent(win);
+    glfwSetFramebufferSizeCallback(win, fbuff_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        throw;
+    }
+    
+    shader_program = compile_shader();
+    conf_vertex(&VAO, &VBO);
+}
+
+bool window::is_open()
+{
+    return !glfwWindowShouldClose(win);
+}
+
+void window::processinput()
+{
+    if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(win, true);
+}
+
+void window::clear_buffer()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void window::draw()
+{
+    glUseProgram(shader_program);
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void window::swap_poll()
+{
+    glfwSwapBuffers(win);
+    glfwPollEvents();
+}
+
+unsigned int window::compile_shader()
+{
+    // Vertex Shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment Shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+void window::conf_vertex(unsigned int* VAO, unsigned int* VBO)
+{
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    }; 
+
+    glGenVertexArrays(1, VAO);
+    glGenBuffers(1, VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(*VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+}
+
+void window::fbuff_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+window::~window()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shader_program);
+    glfwTerminate();
+}
