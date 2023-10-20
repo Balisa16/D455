56c8ac2a32c9cdb5c9ca7e398d864d00e7344b10
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-10-20 15:54:18 +0700

    Add glm

---
+++ test/testlib.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main()
+{
+    glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f, 0.0f);
+    
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *win = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
+    glfwMakeContextCurrent(win);
+
+	std::cout << "Test 1" << std::endl;
+    while(!glfwWindowShouldClose(win))
+    {
+    	glfwPollEvents();
+    }
+    glfwTerminate();
+	return 0;
+}
