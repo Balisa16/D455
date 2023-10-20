ada8364c807606b244e0a5a46a02fa463f1c936d
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-10-20 23:30:04 +0700

    Update CMakeLists.txt

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 build_path="build"
 
 if [ ! -d "$build_path" ]; then
-	echo "Create build folder"
+	printf "\033[32mCreate build folder\033[0m\n"
     mkdir build
 fi
 cd build
