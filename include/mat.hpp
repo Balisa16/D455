45819b9a76f6ec71656e01b4b114eca5a919e106
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-11-03 23:47:22 +0700

    Create math converter

--- include/mat.hpp
+++ include/mat.hpp
@@ -22,10 +22,10 @@ namespace EMIRO
 		float roll2 = roll/2.0f;
 		float pitch2 = pitch/2.0f;
 		float yaw2 = yaw/2.0f;
-	    float qx = sin(roll2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2);
-	    float qy = cos(roll2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2);
-	    float qz = cos(roll2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2);
-	    float qw = cos(roll2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2);
+	    float qx = sin(roll2) * cos(pitch2) * cos(yaw2) - cos(roll2) * sin(pitch2) * sin(yaw2);
+	    float qy = cos(roll2) * sin(pitch2) * cos(yaw2) + sin(roll2) * cos(pitch2) * sin(yaw2);
+	    float qz = cos(roll2) * cos(pitch2) * sin(yaw2) - sin(roll2) * sin(pitch2) * cos(yaw2);
+	    float qw = cos(roll2) * cos(pitch2) * cos(yaw2) + sin(roll2) * sin(pitch2) * sin(yaw2);
 	    return {qx, qy, qz, qw};
 	}
 }
