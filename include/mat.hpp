f937eceba7cbcb4ecbf5d25e6dc0a973eba4676b
Balisa16 <sanderantoniusbalisa16@gmail.com>
2023-11-03 23:57:43 +0700

    Create math converter

--- include/mat.hpp
+++ include/mat.hpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <Eigen/Eigen>
 
 enum class TStatus{
 	None,
@@ -15,9 +16,13 @@ typedef struct{
 	float w, x, y, z;
 }Quaternion;
 
+typedef struct{
+	float roll, pitch, yaw;
+}Euler;
+
 namespace EMIRO
 {
-	Quaternion euler_to_quaternion(float roll, float pitch, float yaw)
+	Eigen::Quaternionf euler_to_quaternion(float roll, float pitch, float yaw)
 	{
 		float roll2 = roll/2.0f;
 		float pitch2 = pitch/2.0f;
@@ -27,5 +32,16 @@ namespace EMIRO
 	    float qz = cos(roll2) * cos(pitch2) * sin(yaw2) - sin(roll2) * sin(pitch2) * cos(yaw2);
 	    float qw = cos(roll2) * cos(pitch2) * cos(yaw2) + sin(roll2) * sin(pitch2) * sin(yaw2);
 	    return {qx, qy, qz, qw};
+	}
+
+	void pclConvert(Position position, Euler euler, pcl::PointCloud<pcl::PointXYZRGB>* in_pointcloud, pcl::PointCloud<pcl::PointXYZRGB>* out_pointcloud)
+	{
+		Eigen::Affine3f transform = Eigen::Affine3f::Identity();
+
+	    transform.translation() << position.x, position.y, position.z;
+	    transform.rotate(Eigen::Quaternionf(qw, qx, qy, qz));
+
+	    // Apply the transformation to the second point cloud
+	    pcl::transformPointCloud(*out_pointcloud, *transformedCloud2, transform);
 	}
 }
